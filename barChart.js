6744171d281532abf30cd4edda92250803bb4880
Sylvain Goedike <sgoedike@videotron.ca>
2018-05-22 20:24:01 +0000

    Refactor all attributes

--- barChart.js
+++ barChart.js
@@ -1,46 +1,42 @@
 
 function drawBarChart(data, options, element){
-  var valueClass = 'value';
-  var valueStyle = 'color: ' + options.barValueColor +
-    '; bottom: ' + options.barValuePos + '%';
-  var labelClass = 'label';
-  var labelStyle = 'color: ' + options.barLabelColor +
-    '; bottom: -30px';
-  var chartTitleStyle = 'color: ' + options.chartTitleColor;
-  var chartStyle = 'width: ' + options.chartWidth + 'px;' +
-    ' height: ' + options.chartHeight + 'px';
   var chartTitleTag = $( options.chartTitleSize ).html(options.chartTitle);
   chartTitleTag.attr({
     'class': 'title',
     'style': 'color: ' + options.chartTitleColor
   });
-  var container = $( element );
-  container.attr('class', 'chart ' + chartStyle);
+  var chartContainer = $( element, {
+    'class': 'chart ' + 'width: ' + options.chartWidth + 'px;' +
+      ' height: ' + options.chartHeight + 'px'
+  });
   var body = $( 'body' );
   chartTitleTag.appendTo(body);
-  container.appendTo(body);
+  chartContainer.appendTo(body);
 
   $.each( data, function( index, value ){
-    var barClass = 'bar ' + options.barColor;
-    var barStyle =  'width: ' + ((options.chartWidth -
-      (options.barSpacing * data.length)) / data.length) +
-      'px; height: ' + value[0] + 'px; ' + 'margin-left: ' +
-      options.barSpacing + 'px; ' + 'margin-right: ' +
-      options.barSpacing + 'px';
     var barTag = $( '<div>', {
-      'class': barClass,
-      'style': barStyle
+      'class': 'bar ' + options.barColor,
+      'style': 'width: ' + ((options.chartWidth -
+        (options.barSpacing * data.length)) / data.length) +
+        'px; height: ' + value[0] + 'px; ' + 'margin-left: ' +
+        options.barSpacing + 'px; ' + 'margin-right: ' +
+        options.barSpacing + 'px'
     });
+
     var valueTag = $( '<div>', {
-      'class': valueClass,
-      'style': valueStyle
+      'class': 'value',
+      'style': 'color: ' + options.barValueColor +
+        '; bottom: ' + options.barValuePos + '%'
     }).html(value[0]);
+
     var labelTag = $( '<div>', {
-      'class': labelClass,
-      'style': labelStyle
+      'class': 'label',
+      'style': 'color: ' + options.barLabelColor +
+        '; bottom: -30px'
     }).html(value[1]);
+
     valueTag.appendTo(barTag);
     labelTag.appendTo(barTag);
-    barTag.appendTo(container);
+    barTag.appendTo(chartContainer);
   });
 }
