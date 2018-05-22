24ebc6b3a1def9c132b1b520d17b42698d642c79
Sylvain Goedike <sgoedike@videotron.ca>
2018-05-22 14:23:25 +0000

    Create chart with color bars

--- barChart.js
+++ barChart.js
@@ -9,6 +9,35 @@ var options = {
 };
 
 function drawBarChart(data, options, element){
+  var barClass = 'bar ' + options.barColor;
+  var valueClass = 'value';
+  var labelClass = 'label color-' + options.barLabelColor;
+  var valueTag = $( '<div>', {
+    'class': valueClass
+  });
+  var labelTag = $( '<div>', {
+    'class': labelClass
+  });
+
+  var chartStyle = 'width: ' + options.chartWidth + 'px;' +
+    ' height: ' + options.chartHeight + 'px';
+  var body = $( 'body' );
+  body.attr('class', 'chart ' + chartStyle);
+
+  $.each( data, function( index, value ){
+    var barStyle =  'width: ' + ((options.chartWidth -
+      (options.barSpacing * data.length)) / data.length) +
+      'px; height: ' + value[0] + 'px; ' + 'margin: ' +
+      options.barSpacing + 'px';
+    var barTag = $( '<div>', {
+      'class': barClass,
+      'style': barStyle
+    });
+    //valueTag.appendTo(barTag);
+    //labelTag.appendTo(barTag);
+    barTag.appendTo(body);
+  });
+
   console.log(options);
   console.log(data[3][1]);
 }
