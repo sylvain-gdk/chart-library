4cea2aec47d3b4c6471a6db8a72c6fde50394fcd
Sylvain Goedike <sgoedike@videotron.ca>
2018-05-22 13:38:02 +0000

    Add options object

--- barChart.js
+++ barChart.js
@@ -1,5 +1,14 @@
-var options = {};
+var options = {
+  chartTitle: 'My Chart',
+  chartWidth: 500,
+  chartHeight: 400,
+  barSpacing: 3,
+  barColor: 'orange',
+  barLabelColor: 'blue',
+  barValuePos: 'bottom'
+};
 
 function drawBarChart(data, options, element){
-
+  console.log(options);
+  console.log(data[3][1]);
 }
