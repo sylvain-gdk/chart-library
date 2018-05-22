ed8f0246f3b6a5c73484b4e5ebd18b55c4319354
Sylvain Goedike <sgoedike@videotron.ca>
2018-05-22 20:07:38 +0000

    Add title to the chart

--- barChart.js
+++ barChart.js
@@ -1,19 +1,31 @@
 
 function drawBarChart(data, options, element){
   var valueClass = 'value';
-  var valueStyle = 'color: ' + options.barValueColor + '; bottom: ' + options.barValuePos + '%';
+  var valueStyle = 'color: ' + options.barValueColor +
+    '; bottom: ' + options.barValuePos + '%';
   var labelClass = 'label';
-  var labelStyle = 'color: ' + options.barLabelColor + '; bottom: -30px';
+  var labelStyle = 'color: ' + options.barLabelColor +
+    '; bottom: -30px';
+  var chartTitleStyle = 'color: ' + options.chartTitleColor;
   var chartStyle = 'width: ' + options.chartWidth + 'px;' +
     ' height: ' + options.chartHeight + 'px';
+  var chartTitleTag = $( options.chartTitleSize ).html(options.chartTitle);
+  chartTitleTag.attr({
+    'class': 'title',
+    'style': 'color: ' + options.chartTitleColor
+  });
+  var container = $( element );
+  container.attr('class', 'chart ' + chartStyle);
   var body = $( 'body' );
-  body.attr('class', 'chart ' + chartStyle);
+  chartTitleTag.appendTo(body);
+  container.appendTo(body);
 
   $.each( data, function( index, value ){
     var barClass = 'bar ' + options.barColor;
     var barStyle =  'width: ' + ((options.chartWidth -
       (options.barSpacing * data.length)) / data.length) +
-      'px; height: ' + value[0] + 'px; ' + 'margin: ' +
+      'px; height: ' + value[0] + 'px; ' + 'margin-left: ' +
+      options.barSpacing + 'px; ' + 'margin-right: ' +
       options.barSpacing + 'px';
     var barTag = $( '<div>', {
       'class': barClass,
@@ -29,6 +41,6 @@ function drawBarChart(data, options, element){
     }).html(value[1]);
     valueTag.appendTo(barTag);
     labelTag.appendTo(barTag);
-    barTag.appendTo(body);
+    barTag.appendTo(container);
   });
 }
