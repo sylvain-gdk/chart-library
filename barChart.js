9ecdf99a15ba6dc70f1986804de12c655028202c
Sylvain Goedike <sgoedike@videotron.ca>
2018-05-28 12:44:54 +0000

    Fix ticks and measures

--- barChart.js
+++ barChart.js
@@ -16,41 +16,67 @@ var options = {
 };
 
 function drawBarChart(data, options, element){
+  var scaleDecrease = 0;
+  var scaleSpace = 0;
+  var scaleStep = 0;
+  var barWidth = (options.chartWidth - (options.barSpacing *
+    data.length)) / data.length;
   var chartTitleTag = $( options.chartTitleSize ).html(options.chartTitle);
-  var measureSpace = 30;
   chartTitleTag.attr({
     'class': 'title',
     'style': 'color: ' + options.chartTitleColor
   });
+
   var chartContainer = $( element, {
     'class': 'chart ' + 'width: ' + options.chartWidth + 'px;' +
       ' height: ' + options.chartHeight + 'px'
   });
+
   var body = $( 'body' );
   chartTitleTag.appendTo(body);
   chartContainer.appendTo(body);
 
-  var measureTag = $( '<div>', {
-    'class': 'measure',
-    'style': 'width: ' + 20 +
-        'px; height: ' + options.chartHeight + 'px; ' + 'margin-right: ' +
-        10 + 'px'
+  var lineTag = $( '<div>', {
+    'class': 'lines',
+    'style': 'width: ' + (options.chartWidth + 30) +
+      'px; height: ' + (options.chartHeight - 25) + 'px'
   });
-  measureSpace = measureSpace + 30;
-  measureTag.appendTo(chartContainer);
+  lineTag.appendTo(chartContainer);
+
+  for(var i = 0; i < 10; i++){
+    var scaleTag = $( '<div>' ).html(function() {
+      var max = 0;
+      $.each( data, function( index, value ){
+        if(max < value[0]){
+          max = value[0];
+        }
+      });
+      scaleStep = max / 10;
+      scaleSpace += scaleStep;
+      if(scaleDecrease < scaleStep){
+        scaleDecrease = scaleStep;
+      }else{
+        scaleDecrease += scaleStep;
+      }
+      return max - scaleDecrease;
+    }).attr({
+      'class': 'measure',
+      'style': 'width: ' + barWidth + 'px; height: '
+      + options.chartHeight + 'px; ' + 'margin-top: ' + scaleSpace  + 'px'
+    });
+    scaleTag.appendTo(chartContainer);
+  }
 
   $.each( data, function( index, value ){
-    var lineHeight = 'color: ' + options.barValueColor +
+    var valueLineHeight = 'color: ' + options.barValueColor +
       '; bottom: ' + options.barValuePos + '%; line-height: ' +
       (value[0] / options.barValuePos) + '%';
 
     var barTag = $( '<div>', {
       'class': 'bar ' + options.barColor,
-      'style': 'width: ' + ((options.chartWidth -
-        (options.barSpacing * data.length)) / data.length) +
-        'px; height: ' + value[0] + 'px; ' + 'margin-left: ' +
-        options.barSpacing + 'px; ' + 'margin-right: ' +
-        options.barSpacing + 'px'
+      'style': 'width: ' + barWidth + 'px; height: ' + value[0] +
+      'px; ' + 'margin-left: ' + options.barSpacing + 'px; ' +
+      'margin-right: ' + options.barSpacing + 'px'
     });
 
     var valueTag = $( '<div>', {
@@ -60,7 +86,7 @@ function drawBarChart(data, options, element){
     }).html(value[0]);
 
     if(options.barValuePos === CENTER){
-      valueTag.attr('style', lineHeight);
+      valueTag.attr('style', valueLineHeight);
     }
 
     var labelTag = $( '<div>', {
@@ -72,5 +98,10 @@ function drawBarChart(data, options, element){
     valueTag.appendTo(barTag);
     labelTag.appendTo(barTag);
     barTag.appendTo(chartContainer);
+
+    console.log('decrease:' + scaleDecrease);
+    console.log('step:' + scaleStep);
+    console.log('space:' + scaleSpace);
+
   });
 }
